464ab065a7234858295beaf715a443bf706baf3f
Damian Melniczuk <damian.melniczuk@gmail.com>
2013-04-18 17:30:13 +0200

    dane z detektorow Alice jako ramka danych

--- skrypt.R
+++ skrypt.R
@@ -1,6 +1,5 @@
 #Wczytywanie przykładowego pliku z danymi:
 surowe_dane <- scan("1-2013-03-27_12:20.txt", character(0), sep = "\n") # separate each word, separate each line
-#Nie wiem jak zainicjować zmienną tak aby nie było błędu więc daję "0" na samym początku:
 przepisane_dane <- vector()
 #Przepisywanie z pominięciem niepotrzebnych linii (póki co tylko 10K bo więcej trochę trwa):
 for (i in 1:10000) {if (grepl("method",surowe_dane[i])) NULL else if (grepl("error",surowe_dane[i])) NULL else if (grepl("signal",surowe_dane[i])) NULL else if (grepl("OID",surowe_dane[i])) NULL else przepisane_dane=c(przepisane_dane,surowe_dane[i]) }
@@ -14,13 +13,16 @@ dca_raw_clean <- vector()
 for (i in 1: length(dca_raw)) {dca_raw_clean[i] <- substring(dca_raw[i],26,nchar(dca_raw[i])-1) }
 
 dca_raw_clean
-df_adet <- data.frame(adet1,adet2,adet3,adet4)
-df_adet
+df_adet <- data.frame(adet1=integer(0),adet2=integer(0),adet3=integer(0),adet4=integer(0))
+#df_adet
 
-for (i in 1: length(dca_raw_clean)) {testaaa<-strsplit(dca_raw_clean[i],";");
-                                     det1<-testaaa[[1]][1];
-                                     det2<-testaaa[[1]][2];
-                                     det3<-testaaa[[1]][3];
-                                     det4<-testaaa[[1]][4];
-                                     df_adet<-rbind(df_adet,c(det1,det2,det3,det4))}
+for (i in 1: length(dca_raw_clean)) {tempor<-strsplit(dca_raw_clean[i],";");
+                                     det1<-as.integer(tempor[[1]][1]);
+                                     det2<-as.integer(tempor[[1]][2]);
+                                     det3<-as.integer(tempor[[1]][3]);
+                                     det4<-as.integer(tempor[[1]][4]);
+                                     df_adet <-rbind(df_adet,c(det1,det2,det3,det4));
+                                      }
+colnames(df_adet) <- c("adet1","adet2","adet3","adet4")
 df_adet
+summary(df_adet)
