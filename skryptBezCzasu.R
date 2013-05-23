c187fc3250ee32fe92fbc23334d21ec04b21b857
Damian Melniczuk <damian.melniczuk@gmail.com>
2013-05-23 15:11:06 +0200

    skrypt przelatuje teraz pliki .txt z danego katalogu i generuje .csv i .eps

--- skryptBezCzasu.R
+++ skryptBezCzasu.R
@@ -1,7 +1,13 @@
+#rozpoczecie iteracji po plikach jak tu: http://stackoverflow.com/a/4855916/993324
+file.dir <- "~/Dropbox/quelle/bezCzasu/"
+for(infile in dir(file.dir, pattern="\\.txt$")) {
+  outfile_csv <- gsub("\\.txt","\\.csv", infile)
+  outfile_eps <- gsub("\\.txt","\\.eps", infile)
+
 przepisane_dane <- vector()
 qber_data_frame <- data.frame()
 
-surowe_dane <- scan("~/Dropbox/quelle/bezCzasu/2013-05-13_10:45.txt", character(0), sep = "\n")
+surowe_dane <- scan(paste(file.dir,infile,sep=""), character(0), sep = "\n")
 for (i in 1: length(surowe_dane) ) {if (grepl("method",surowe_dane[i])) NULL else if (grepl("error",surowe_dane[i])) NULL else if (grepl("signal",surowe_dane[i])) NULL else if (grepl("OID",surowe_dane[i])) NULL else przepisane_dane=c(przepisane_dane,surowe_dane[i]) }
 
 for (i in 1:length(przepisane_dane)) {if (grepl("qber",przepisane_dane[i]) & grepl("variant",przepisane_dane[i+1])) qber_data_frame<-rbind(qber_data_frame,c(i+1,as.numeric(substring(przepisane_dane[i+1],25,nchar(przepisane_dane[i+1])))))}
@@ -9,10 +15,14 @@ for (i in 1:length(przepisane_dane)) {if (grepl("qber",przepisane_dane[i]) & gre
 qber_data_frame
 colnames(qber_data_frame) <- c("line", "qber")
 qber_data_frame<-qber_data_frame[qber_data_frame$qber!=0, ]
-write.csv(qber_data_frame,"~/Dropbox/quelle/bezCzasu/2013-05-13_10:45.qber.csv")
+write.csv(qber_data_frame,paste(file.dir,outfile_csv,sep=""))
 
 library(ggplot2)
 
 p <- ggplot(qber_data_frame, aes(x=line, y=qber))
 p <- p+geom_line()
 p
+ggsave(file=paste(file.dir,outfile_eps,sep=""))
+
+#zakonczenie iteracji po plikach:
+}
